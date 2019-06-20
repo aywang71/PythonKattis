28c249952f50c6a2c3227174d47685d78386d45c
Hyperwizard42 <44416801+Hyperwizard42@users.noreply.github.com>
2019-06-20 11:43:34 -0400

    Create planina.py

---
+++ code/planina.py
@@ -0,0 +1,14 @@
+#input format
+n = int(input())
+#can't calculate for the first iteration: if loop
+if n == 1:
+  print(9)
+else:
+  # if n!= 1, run this loop
+  i=3
+  #counts each iteration of i in range of x
+  for x in range (1,n):
+    i = i+(i-1)
+    #debugging
+    print(i)
+  print(i*i)

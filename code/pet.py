f0baa6e655da035d834905f7532db68c5f893b8e
Andrew Wang <hyperwizard42@gmail.com>
2020-10-23 13:59:32 +0000

    updated pet.py to better code

--- code/pet.py
+++ code/pet.py
@@ -1,35 +1,13 @@
-#https://open.kattis.com/problems/pet
-#input format
-a = input().split()
-b = input().split()
-c = input().split()
-d = input().split()
-e = input().split()
-#converts values to integer
-for i in range (0,len(a)):
-  a[i] = int(a[i])
-for i in range (0,len(b)):
-  b[i] = int(b[i])
-for i in range (0,len(c)):
-  c[i] = int(c[i])
-for i in range (0,len(d)):
-  d[i] = int(d[i])
-for i in range (0,len(e)):
-  e[i] = int(e[i])
-#new storage values to sum a to e
-f = sum(a)
-g = sum(b)
-h = sum(c)
-i = sum(d)
-j = sum(e)
-l = []
-#appends all of the storage values
-l.append(f)
-l.append(g)
-l.append(h)
-l.append(i)
-l.append(j)
-#output format
-w = l.index(max(l))
-w += 1
-print (w,max(l))
+maxScore = 0
+num = 0;
+
+for j in range(5):
+  x = input().split(' ')
+  for i in range(len(x)):
+    x[i] = int(x[i])
+  #print(x, sum(x))
+  if sum(x) > maxScore:
+    maxScore = sum(x)
+    #print("j:", j+1)
+    num = j + 1
+print(num, maxScore)
